ad7273df0ad4ff1184bb951d24028ecd934283f7
Marcel Rodekamp <marcel.rodekamp@gmail.com>
2021-07-09 08:44:29 +0200

    Empty Basics Section

---
+++ Basics.hpp
@@ -0,0 +1,28 @@
+#ifndef CPPCOURSE_BASICS_HPP
+#define CPPCOURSE_BASICS_HPP
+
+namespace Basics{
+namespace DataTypes{
+
+}
+namespace VariableAndScope{
+
+}
+namespace ConstantsAndLiterals{
+
+}
+namespace AccessModifiers{
+
+}
+namespace StorageClasses{
+
+}
+namespace ForwardDeclaration {
+
+}
+namespace ErrorHandling{
+
+}
+}
+
+#endif //CPPCOURSE_BASICS_HPP
